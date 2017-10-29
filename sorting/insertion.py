0c2c4b7f1b5d65a7604be574d4d7125790b27c83
Brianna Hirsch <bhirsch@asu.edu>
2017-10-28 18:43:10 -0700

    Added comments to Insertion

--- sorting/insertion.py
+++ sorting/insertion.py
@@ -1,7 +1,10 @@
 def insertion(numbers):
-	for i in range(1, len(numbers)):
+        for i in range(1, len(numbers)):
 		j = i
+                #While loop to check if previous element is larger 
+                #If previous element is smaller, continue traversing
 		while j > 0 and numbers[j-1] > numbers[j]:
+                        #Swap elements, decrement j
                         numbers[j], numbers[j-1] = numbers[j-1], numbers[j]
                         j -= 1
 	return numbers
